01b413b7dd7489a955d80db31ecc9de9693522fe
Deepanshu Dhruw <deepanshu.dhruw@outlook.com>
2020-10-20 01:52:52 +0530

    Delete env_sample.php

--- env_sample.php
+++
@@ -1,9 +0,0 @@
-<?php
-$variables = [
-    "API_KEY" => "YOUR NEWS.API.ORG API",
-    "BASE_URL" => "YOUR_BASE_URL", #Example: http://localhost
-    "FOLDER" => "PROJECT_FOLDER_NAME" #Example: /News
-];
-foreach ($variables as $key => $val) {
-    putenv("$key=$val");
-}
